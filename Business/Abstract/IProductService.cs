d4ccceccc6f907c9d636f4b3009e5544aeaf1459
Serkan Özmen <ozmens@gmail.com>
2021-03-11 00:29:58 +0300

    10 Mart Ders Sonu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -21,7 +21,7 @@ namespace Business.Abstract
 
         IResult Update(Product product);
 
-        IResult AddTransactionalTest(Product product)
+        IResult AddTransactionalTest(Product product);
 
         //RESTFUL --> HTTP --> 
 
